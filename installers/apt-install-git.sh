9e7b690e4d9dc6aff199f7f4999eb9b6858780d4
Eduardo Eljaiek <eduardo.eljaiek@gmail.com>
2020-05-08 11:18:44 +0300

    feat: add git(apt) installer

---
+++ installers/apt-install-git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+:'
+If add-apt-repository command is not found, install it first with:
+
+sudo apt-get install software-properties-common python-software-properties
+'
+
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git

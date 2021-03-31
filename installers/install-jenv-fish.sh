8acb396acf71116111a7074e5dfc2ca6821c8960
eduardo.eljaiek <eduardo.eljaiek@gmail.com>
2021-03-31 22:04:23 +0300

    fix(jenv-fish): function directory isn't created

--- installers/install-jenv-fish.sh
+++ installers/install-jenv-fish.sh
@@ -6,7 +6,7 @@ git clone https://github.com/jenv/jenv.git ~/.jenv
 
 echo 'set PATH $HOME/.jenv/bin $PATH' >> ~/.config/fish/config.fish
 echo 'status --is-interactive; and source (jenv init -|psub)' >> ~/.config/fish/config.fish
-echo mkdir ~/.config/fish/function
+mkdir ~/.config/fish/function
 cp ~/.jenv/fish/jenv.fish ~/.config/fish/function/jenv.fish
 cp ~/.jenv/fish/export.fish ~/.config/fish/function/export.fish
 

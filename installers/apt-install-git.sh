bc0cba1c56c502105e59953f647070117cd7bb3b
Eduardo Eljaiek Rodriguez <eljaiek@users.noreply.github.com>
2021-05-31 16:04:20 +0300

    refactor: install git only if not present

--- installers/apt-install-git.sh
+++ installers/apt-install-git.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-'
+: '
 If add-apt-repository command is not found, install it first with:
 
 sudo apt-get install software-properties-common python-software-properties
 '
 
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update
-sudo apt-get install git
+if ! type "git" > /dev/null; then
+    sudo add-apt-repository ppa:git-core/ppa
+    sudo apt-get update
+    sudo apt-get install git -y
+else
+    git --version
+fi   

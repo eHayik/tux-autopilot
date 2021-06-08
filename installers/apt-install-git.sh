6a5f351f5797a4206de452b82136ba865752e96d
eHayik <eduardo.eljaiek@gmail.com>
2021-06-08 13:59:18 +0300

    feat(install git):  setting global git username and password

--- installers/apt-install-git.sh
+++ installers/apt-install-git.sh
@@ -10,6 +10,10 @@ if ! type "git" > /dev/null; then
     sudo add-apt-repository ppa:git-core/ppa
     sudo apt-get update
     sudo apt-get install git -y
+
+    git config --global user.name $USER_NAME
+    git config --global user.email $USER_MAIL
+
 else
     git --version
 fi   

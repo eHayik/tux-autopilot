5c3e7b9aef60528001c0a748da6e2bd7b65aeb3d
eHayik <eduardo.eljaiek@gmail.com>
2021-06-10 16:57:45 +0300

    feat(install): fish shell plugins

---
+++ installers/fish/install-spacefish.sh
@@ -0,0 +1,11 @@
+#!/bin/fish
+
+: '
+Spacefish is a minimalistic, powerful and extremely customizable Fish Shell prompt. 
+https://github.com/matchai/spacefish
+'
+
+sudo apt install fonts-powerline -y
+
+fisher install matchai/spacefish
+ 
---
+++ installers/fish/install-z.sh
@@ -0,0 +1,10 @@
+#!/bin/fish
+
+: '
+z tracks the directories you visit. With a combination of frequency and recency, 
+it enables you to jump to the directory in mind.
+https://github.com/jethrokuan/z
+'
+
+fisher install jethrokuan/z
+

dd4e6ca3733bcab79f0da1e1cf39b31ee1bc4f77
eHayik <eduardo.eljaiek@gmail.com>
2021-09-04 09:27:22 +0300

    doc: add link to the guidance to run docker as root

--- docker/apt-install-docker.sh
+++ docker/apt-install-docker.sh
@@ -39,9 +39,16 @@ sudo add-apt-repository \
  # Verify that Docker Engine is installed correctly by running the hello-world image.
  sudo docker run hello-world
 
+
+
+
  # 4. Install Docker Compose
  sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
  
  sudo chmod +x /usr/local/bin/docker-compose
 
  docker-compose --version
+
+
+# Run Docker as non root user without sudo command
+# https://www.configserverfirewall.com/docker/run-docker-without-sudo/

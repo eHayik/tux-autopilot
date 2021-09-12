dd2d4fd79253c8d660fd297b679483e5ffbef1fc
ehayik <eduardo.eljaiek@gmail.com>
2021-09-12 23:08:47 +0300

    edit(fish config): add more alias to fish config

--- installers/fish/config.fish
+++ installers/fish/config.fish
@@ -1,9 +1,31 @@
 set -U fish_greeting '><(((">'
 
-if ! type "code" > /dev/null;
-    abbr -a cf 'code ~/.config/fish/config.fish'
+# Tools
+abbr -a nf 'nano ~/.config/fish/config.fish'
+abbr -a cf 'code ~/.config/fish/config.fish'
+abbr -a ll 'ls -laht'
+alias nuke='sudo rm -rvf $argv'
+alias gzd='gzip -dk $argv'
+alias td='tar -cvf $argv'
+alias fkc='flatpak uninstall --unused'
+
+function cl --wraps=clear --description 'alias cl clear'
+  clear $argv;
 end
 
-if ! type "nano" > /dev/null;
-    abbr -a nf 'nano ~/.config/fish/config.fish'
+function mkcd -d "Create a directory and set CWD"
+    command mkdir -p $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
+
+# Gradle
+function guw -d "update gradle wrapper to the given version"
+ ./gradlew wrapper --gradle-version $argv  --distribution-type bin
 end

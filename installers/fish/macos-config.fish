05704589aa0643b53aa12769e438f92dc2458cf3
ehayik <ehayik@tutanota.com>
2023-12-20 12:02:25 +0200

    Backup macos fish config

--- installers/fish/macos-config.fish
+++ installers/fish/macos-config.fish
@@ -1,20 +1,39 @@
-### ADDING TO THE PATH
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
+### ADDING TO THE PATH
 # First line removes the path; second line sets it. Without the first line,
 # your path gets massive and fish becomes very slow.
 set -e fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
 
+### EXPORT ###
+
+## Default Editor
+set -x -g EDITOR "/opt/homebrew/bin/micro"
+
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 ## Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
 fish_add_path "/opt/homebrew/bin/"
 
-### END OF ADDING TO THE PATH ### 
+## Add JetBrains shell scripts location 
+fish_add_path "/Users/ehayik/Library/Application Support/JetBrains/Toolbox/scripts"
 
-### EXPORT ###
+## Java Setup
+set -x -g JAVA_17_HOME (/usr/libexec/java_home -v 17)
+set -x -g JAVA_11_HOME (/usr/libexec/java_home -v 11)
+set -x -g JAVA_8_HOME (/usr/libexec/java_home -v 1.8)
+
+abbr -a java17 'set -x -g JAVA_HOME $JAVA_17_HOME'
+abbr -a java11 'set -x -g JAVA_HOME $JAVA_11_HOME'
+abbr -a java8 'set -x -g JAVA_HOME $JAVA_8_HOME'
 
 ## Maven
-#set -x MAVEN_OPTS "-Djava.net.preferIPv4Stack=true -XX:-TieredCompilation -XX:TieredStopAtLevel=1"
+set -x MAVEN_OPTS "-Djava.net.preferIPv4Stack=true -XX:-TieredCompilation -XX:TieredStopAtLevel=1"
+
+## Docker Remote Host
+set -x -g DOCKER_HOST "ssh://Ehayik@192.168.100.78"
 
 ### END OF EXPORT ### 
 
@@ -93,24 +112,35 @@ alias pull='git pull origin'
 alias push='git push origin'
 alias tag='git tag'
 alias newtag='git tag -a'
+alias gclean='git clean -xdf'
 
 ## files
 abbr -a ll 'ls -laht'
 alias nuke='sudo rm -rvf $argv'
 
-## packages
-
+## brew
 abbr -a install 'brew install'
-abbr -a  update 'brew update'
-abbr -a  upgrade 'brew upgrade'
+abbr -a update 'brew update'
+abbr -a upgrade 'brew upgrade'
 abbr -a tap 'brew tap'
 abbr -a untap 'brew untap'
 abbr -a cask-install 'brew install --cask'
 abbr -a cask-uninstall 'brew uninstall --cask'
 abbr -a cask-upgrade 'brew cask upgrade'
 
+## Espanso
+abbr -a ce 'code ~/Library/Application\ Support/espanso/match/base.yml'
+
+## Docker
+abbr -a  docker-start 'colima start'
+abbr -a  docker-stop 'colima stop'
+
 ## Enable Startship prompt
-# starship init fish | source
+starship init fish | source
 
 ## Enable Jenv
-#status --is-interactive; and source (jenv init --no-rehash -|psub)
+status --is-interactive; and source (jenv init --no-rehash -|psub)
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/ehayik/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
